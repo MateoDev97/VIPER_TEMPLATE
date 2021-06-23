f9041f7e0902d0611ad3b3a962145f5a2b1dac4d
MateoDev97 <mateo972000@gmail.com>
2021-06-23 00:43:34 -0500

    first commit

---
+++ VIPER CFEAPPS.xctemplate/___FILEBASENAME___Protocols.swift
@@ -0,0 +1,55 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import Foundation
+import UIKit
+
+protocol ___VARIABLE_ModuleName___ViewProtocol: AnyObject {
+    // PRESENTER -> VIEW
+    var presenter: ___VARIABLE_ModuleName___PresenterProtocol? { get set }
+}
+
+protocol ___VARIABLE_ModuleName___RouterProtocol: AnyObject {
+    // PRESENTER -> WIREFRAME
+    static func create___VARIABLE_ModuleName___Module() -> UIViewController
+}
+
+protocol ___VARIABLE_ModuleName___PresenterProtocol: AnyObject {
+    // VIEW -> PRESENTER
+    var view: ___VARIABLE_ModuleName___ViewProtocol? { get set }
+    var interactor: ___VARIABLE_ModuleName___InteractorInputProtocol? { get set }
+    var router: ___VARIABLE_ModuleName___RouterProtocol? { get set }
+    
+    func viewDidLoad()
+}
+
+protocol ___VARIABLE_ModuleName___InteractorOutputProtocol: AnyObject {
+// INTERACTOR -> PRESENTER
+}
+
+protocol ___VARIABLE_ModuleName___InteractorInputProtocol: AnyObject {
+    // PRESENTER -> INTERACTOR
+    var presenter: ___VARIABLE_ModuleName___InteractorOutputProtocol? { get set }
+}
+
+protocol ___VARIABLE_ModuleName___DataManagerInputProtocol: AnyObject {
+    // INTERACTOR -> DATAMANAGER
+}
+
+protocol ___VARIABLE_ModuleName___RemoteDataManagerInputProtocol: AnyObject {
+    // INTERACTOR -> REMOTEDATAMANAGER
+    var remoteRequestHandler: ___VARIABLE_ModuleName___RemoteDataManagerOutputProtocol? { get set }
+}
+
+protocol ___VARIABLE_ModuleName___RemoteDataManagerOutputProtocol: AnyObject {
+    // REMOTEDATAMANAGER -> INTERACTOR
+}
+
+protocol ___VARIABLE_ModuleName___LocalDataManagerInputProtocol: AnyObject {
+    // INTERACTOR -> LOCALDATAMANAGER
+}
